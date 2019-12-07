e2bced13496f5d707d7b0d4e458b8f066a465087
VladLisin <vladlisin2@gmail.com>
2019-12-07 21:36:14 +0700

    11232019

--- service_core/settings.py
+++ service_core/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'b_=69lzr9o=3c*j+fea=(v!*9)=b(dgdpt)d#5(hg%9f-(-07p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
