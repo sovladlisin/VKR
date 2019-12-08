4bd0605e782c448f3d2f271cbdca8a87449d68b3
VladLisin <vladlisin2@gmail.com>
2019-12-08 23:07:40 +0700

    12082019

--- annotation_tool/urls.py
+++ annotation_tool/urls.py
@@ -5,16 +5,8 @@ from . import views
 app_name = 'annotation_tool'
 urlpatterns = [
     url(r'^workspace/(?P<pk>\d+)/edit/$', views.Workspace, name='workspace'),
-    path('infoWindow', views.InfoWindow, name='infoWindow'),
-    path('searchWindow', views.SearchWindow, name='searchWindow'),
-    path('search', views.Search, name='search'),
-    path('tree', views.ClassTree, name='tree'),
-    path('saveWindow', views.SaveWindow, name='saveWindow'),
     path('blocks', views.BlockSelection, name='blocks'),
     path('uploadDOCX', views.UploadDOCX, name='uploadDOCX'),
-    path('pinFactory', views.PinFactory, name='pinFactory'),
-    path('pinFactoryWindow', views.PinFactoryWindow, name='pinFactoryWindow'),
-    # path('saveEntity', views.SaveEntity, name='saveEntity'),
     url(r'^save/(?P<pk>\d+)/(?P<model>\w+)/Entity/$',
         views.SaveEntity, name='saveEntity'),
     path('getLineDependencies', views.getLineDependencies,
